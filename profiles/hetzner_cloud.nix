8cca6bf8b1c0397ec5b1a2e428410a2e429dfef4
Boberg <blazed@darkstar.se>
2021-10-02 18:25:07 +0200

    add systemd-boot false

--- profiles/hetzner_cloud.nix
+++ profiles/hetzner_cloud.nix
@@ -1,10 +1,14 @@
-{ modulesPath, ... }:
+{ modulesPath, lib, ... }:
+let 
+  inherit (lib) mkForce;
+in
 {
   imports = [
     (modulesPath + "/profiles/qemu-guest.nix")
   ];
 
   boot = {
+    loader.systemd-boot.enable = mkForce false;
     loader.grub.enable = true;
     loader.grub.version = 2;
     loader.grub.devices = [ "/dev/sda" ];
