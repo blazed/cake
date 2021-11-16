ea0298caefe74a637a42e30b6e203cdcce03876d
Boberg <blazed@darkstar.se>
2021-11-16 10:57:03 +0100

    fix sha

--- packages/k3s-io/metadata.nix
+++ packages/k3s-io/metadata.nix
@@ -1,5 +1,5 @@
 {
   url = "https://github.com/k3s-io/k3s/releases/download/v1.22.3+k3s1/k3s";
   version = "v1.22.3+k3s1";
-  sha256 = "b22f0a1c18a62b58137759d419a0e077eed640c6784425756c5f04910d6a6866";
+  sha256 = "sha256-Z1VigkTo/63TQ1Um09sRbPFken+QkIYctVBkftMEVEU=";
 }
