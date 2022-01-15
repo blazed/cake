cc42a21657eb7893689d1e0e05e617bfc0c99ee7
Boberg <blazed@darkstar.se>
2022-01-15 22:54:42 +0100

    k3s: update to v1.22.5

--- packages/k3s-io/metadata.nix
+++ packages/k3s-io/metadata.nix
@@ -1,5 +1,5 @@
 {
-  url = "https://github.com/k3s-io/k3s/releases/download/v1.22.3+k3s1/k3s";
-  version = "v1.22.3+k3s1";
+  url = "https://github.com/k3s-io/k3s/releases/download/v1.22.5+k3s2/k3s";
+  version = "v1.22.5+k3s2";
   sha256 = "0rk8d86r212zdisjai3qqr0ddvkpw2h1km2rfw9mhax630f0lbxj";
 }
