a9e5161f0f0f9c5cc3718ecee59825d941bb9fcc
Boberg <blazed@users.noreply.github.com>
2022-04-02 02:39:34 +0200

    private: make sure it works on laptop (#172)

--- packages/overlays.nix
+++ packages/overlays.nix
@@ -80,7 +80,7 @@ in
   swaylock-dope = (final: prev: { swaylock-dope = prev.callPackage ./swaylock-dope { }; });
   wl-clipboard-x11 = (final: prev: { wl-clipboard-x11 = prev.callPackage ./wl-clipboard-x11 { }; });
   rust-analyzer-bin = (final: prev: { rust-analyzer-bin = prev.callPackage ./wl-clipboard-x11 { }; });
-  netns-dbus-proxy = (final: prev: { netns-dbus-proxy = prev.callPackage ./wl-clipboard-x11 { }; });
+  netns-dbus-proxy = (final: prev: { netns-dbus-proxy = prev.callPackage ./netns-dbus-proxy { }; });
   scripts = (final: prev: { scripts = prev.callPackage ./scripts { }; });
 }
 
