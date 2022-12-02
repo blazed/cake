2bc3618f7d8023e9265b38a5042a2bd829b60963
Boberg <blazed@darkstar.se>
2022-12-02 23:39:33 +0100

    Add innernet

--- profiles/extra-hosts.nix
+++ profiles/extra-hosts.nix
@@ -1,11 +1,11 @@
 {
   networking.extraHosts = ''
     10.0.0.20 media
-    10.0.149.3 diana
-    10.0.149.5 angelica
-    10.0.149.6 nicolina
-    10.0.149.20 sophia
-    10.0.149.21 elsa
-    10.0.149.22 freja
+    10.0.153.3 diana
+    10.0.152.1 angelica
+    10.0.153.6 nicolina
+    10.0.153.20 sophia
+    10.0.153.21 elsa
+    10.0.153.22 freja
   '';
 }
