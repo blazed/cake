95cf587dbbd3b7e0cbdfb6ccd80b5d6db766128f
Boberg <blazed@darkstar.se>
2023-01-15 11:48:48 +0100

    Some fixes

--- profiles/hardware/nuc.nix
+++ profiles/hardware/nuc.nix
@@ -2,4 +2,7 @@
   imports = [
     ./intel.nix
   ];
+
+  fileSystems."/nix".options = ["subvol=@nix" "rw" "noatime" "compress=zstd"];
+  fileSystems."/keep".options = ["subvol=@keep" "rw" "noatime" "compress=zstd"];
 }
