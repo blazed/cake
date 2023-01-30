0efe8b436e232a20c3fee688df1f40b658cf027e
Boberg <blazed@darkstar.se>
2023-01-30 18:39:13 +0100

    Enable oidc for k3s

---
+++ profiles/acme.nix
@@ -0,0 +1,9 @@
+{lib, ...}: {
+  security.acme.acceptTerms = true;
+  security.acme.defaults = {
+    email = "certs@exsules.com";
+    dnsResolver = "1.1.1.1:53";
+    dnsProvider = "cloudflare";
+    credentialsFile = "/run/agenix/acme-cloudflare";
+  };
+}
