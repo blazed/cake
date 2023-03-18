444aea460ac208343e7eeabcd182714120573682
Boberg <blazed@darkstar.se>
2023-03-18 07:32:49 +0100

    Different boot opts

--- profiles/hardware/intel.nix
+++ profiles/hardware/intel.nix
@@ -1,4 +1,7 @@
 {
   hardware.cpu.intel.updateMicrocode = true;
   boot.kernelModules = ["kvm-intel"];
+
+  fileSystems."/nix".options = ["subvol=@nix" "rw" "noatime" "compress=zstd"];
+  fileSystems."/keep".options = ["subvol=@keep" "rw" "noatime" "compress=zstd"];
 }
