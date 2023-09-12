7f52f936a9f8f2d53de3d53537e65cfdd09ea196
Boberg <1823919+blazed@users.noreply.github.com>
2023-09-13 00:28:09 +0200

    Remove hosts (#417)
    * Delete jolene and olivia
    
    * Cleanup, delete unused hosts and secrets

---
+++ users/profiles/nushell/default.nix
@@ -0,0 +1,22 @@
+{
+  pkgs,
+  lib,
+  config,
+  ...
+}: let
+  inherit (config) home;
+in {
+  programs.direnv.enableNushellIntegration = false;
+  programs.nushell = {
+    enable = true;
+    package = pkgs.nushell;
+    configFile.source = ./config.nu;
+    envFile.source = ./env.nu;
+  };
+  xdg.configFile."nushell/starship.nu".source = ./starship.nu;
+  xdg.configFile."nushell/home.nu".source = pkgs.writeText "home.nu" ''
+    ${
+      lib.concatStringsSep "\n" (lib.mapAttrsToList (name: value: "$env.${name} = \"${value}\";") home.sessionVariables)
+    }
+  '';
+}
