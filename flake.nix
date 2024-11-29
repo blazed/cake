80f2a200d90602f4d2f6cef2109776664dbeeb04
Boberg <1823919+blazed@users.noreply.github.com>
2024-11-29 17:37:08 +0100

    rollback to using `nixos-unstable` instead of `nixos-unstable-small`

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
     nixos-generators.inputs.nixpkgs.follows = "nixpkgs";
     nixos-generators.url = "github:nix-community/nixos-generators";
     nixos-hardware.url = "github:nixos/nixos-hardware";
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable-small";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     persway.inputs.crane.follows = "crane";
     persway.inputs.devenv.follows = "devenv";
