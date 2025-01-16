a946f88c831cd2cc98c9ca1d89d397eaff0bd65c
Boberg <1823919+blazed@users.noreply.github.com>
2025-01-16 20:45:35 +0100

    feat(amelia): enable fingerprint support (#579)

--- profiles/hardware/framework-13-amd.nix
+++ profiles/hardware/framework-13-amd.nix
@@ -1,14 +1,11 @@
-{
-  inputs,
-  lib,
-  ...
-}: {
+{inputs, ...}: {
   imports = [
     inputs.nixos-hardware.nixosModules.framework-13-7040-amd
   ];
 
   boot.initrd.availableKernelModules = ["nvme" "xhci_pci" "thunderbolt" "usb_storage" "sd_mod"];
-  services.tlp.enable = lib.mkForce false;
+  services.fprintd.enable = true;
+
   environment.persistence."/keep" = {
     directories = ["/var/lib/fprint"];
   };
