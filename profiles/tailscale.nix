0e65836842a8129b0d0834ebaf9fd852168c4e09
Boberg <1823919+blazed@users.noreply.github.com>
2025-09-13 20:17:55 +0200

    feat(k3s): enable cilium host namespace bypass (#804)

--- profiles/tailscale.nix
+++ profiles/tailscale.nix
@@ -1,9 +1,7 @@
-{ pkgs, ... }:
 {
   services.tailscale = {
     enable = true;
     interfaceName = "tailscale0";
-    package = pkgs.tailscale.overrideAttrs { doCheck = false; };
   };
   networking.firewall.trustedInterfaces = [ "tailscale0" ];
   networking.firewall.checkReversePath = "loose";
