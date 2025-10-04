779cf7457e75eff472b616d7e6ba8f92a044ae49
Boberg <1823919+blazed@users.noreply.github.com>
2025-10-04 16:13:13 +0200

    fix: deprecated things (#823)

--- profiles/defaults.nix
+++ profiles/defaults.nix
@@ -165,7 +165,7 @@ in
     setuid = true;
   };
 
-  system.stateVersion = "24.05";
+  system.stateVersion = "25.05";
 
   system.activationScripts.agenixNewGeneration = mkIf (hasSecrets && hasState) {
     deps = [ "persist-files" ];
