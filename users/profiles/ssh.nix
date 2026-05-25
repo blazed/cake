2991f263a393eb78b0332c984fe1e6b1247f5742
Boberg <1823919+blazed@users.noreply.github.com>
2026-05-25 16:16:06 +0200

    fix: silence nix check evaluation warnings
    Signed-off-by: Boberg <1823919+blazed@users.noreply.github.com>

--- users/profiles/ssh.nix
+++ users/profiles/ssh.nix
@@ -1,46 +1,41 @@
+let
+  # Shared policy for git forges: key-only auth, no connection multiplexing.
+  gitForge = {
+    User = "git";
+    ForwardAgent = false;
+    PreferredAuthentications = "publickey";
+    ControlMaster = "no";
+    ControlPath = "none";
+  };
+in
 {
   programs.ssh = {
     enable = true;
     enableDefaultConfig = false;
 
-    matchBlocks = {
+    settings = {
       "*" = {
-        controlPersist = "30m";
-        controlMaster = "auto";
-        controlPath = "~/.ssh/master-%r@%n:%p";
-        serverAliveInterval = 60;
-        serverAliveCountMax = 3;
-        hashKnownHosts = false;
-        forwardAgent = true;
-        addKeysToAgent = "no";
-        compression = false;
+        ControlPersist = "30m";
+        ControlMaster = "auto";
+        ControlPath = "~/.ssh/master-%r@%n:%p";
+        ServerAliveInterval = 60;
+        ServerAliveCountMax = 3;
+        HashKnownHosts = false;
+        ForwardAgent = true;
+        AddKeysToAgent = "no";
+        Compression = false;
       };
-      "github github.com" = {
-        hostname = "github.com";
-        user = "git";
-        forwardAgent = false;
-        extraOptions = {
-          preferredAuthentications = "publickey";
-          controlMaster = "no";
-          controlPath = "none";
-        };
+      "github github.com" = gitForge // {
+        HostName = "github.com";
       };
-      "git.exsules.com git.exsules.dev" = {
-        user = "git";
-        forwardAgent = false;
-        extraOptions = {
-          preferredAuthentications = "publickey";
-          controlMaster = "no";
-          controlPath = "none";
-        };
-      };
+      "git.exsules.com exsules.dev" = gitForge;
       "192.168.122.141" = {
-        user = "kaziri";
-        forwardAgent = false;
+        User = "kaziri";
+        ForwardAgent = false;
       };
       "nicolina" = {
-        user = "blazed";
-        forwardAgent = true;
+        User = "blazed";
+        ForwardAgent = true;
       };
     };
   };
