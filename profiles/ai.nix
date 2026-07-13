b49d7e3b5e3137c438d14ae1271fadcf36760ca5
Boberg <1823919+blazed@users.noreply.github.com>
2026-07-13 23:17:44 +0000

    chore(ai): update llama-swap and llama.cpp

--- profiles/ai.nix
+++ profiles/ai.nix
@@ -61,12 +61,12 @@
             rocmGpuTargets = [ "gfx1151" ];
           }).overrideAttrs
             (oa: rec {
-              version = "9978";
+              version = "9993";
               src = pkgs.fetchFromGitHub {
                 owner = "ggml-org";
                 repo = "llama.cpp";
                 tag = "b${version}";
-                hash = "sha256-HzhiiSn+ic6E/EPR9ri9pXFSak699fKCAoAPX3OaBcU=";
+                hash = "sha256-cAskC4wMrWuAxdHSn2Y3t9HYWwp//HlZbuespLPYAHY=";
                 leaveDotGit = true;
                 postFetch = ''
                   git -C "$out" rev-parse --short HEAD > $out/COMMIT
