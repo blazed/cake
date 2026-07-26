5056c5251c2e95c9a7e0277f8d2e5338d9333588
Boberg <1823919+blazed@users.noreply.github.com>
2026-07-26 11:00:13 +0200

    fix(proton-pass): use D-Bus keyring backend
    Signed-off-by: Boberg <1823919+blazed@users.noreply.github.com>

--- users/profiles/protonpass.nix
+++ users/profiles/protonpass.nix
@@ -1,4 +1,11 @@
 { pkgs, ... }:
 {
-  home.packages = [ pkgs.proton-pass-cli ];
+  home = {
+    packages = [ pkgs.proton-pass-cli ];
+
+    sessionVariables = {
+      PROTON_PASS_KEY_PROVIDER = "keyring";
+      PROTON_PASS_LINUX_KEYRING = "dbus";
+    };
+  };
 }
