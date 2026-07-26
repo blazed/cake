b78b0849fc83aca24d7225b65543094633d94399
Boberg <1823919+blazed@users.noreply.github.com>
2026-07-26 15:03:31 +0200

    feat(agents): enable Proton Pass in jailed Pi
    Signed-off-by: Boberg <1823919+blazed@users.noreply.github.com>

--- hosts/x86_64-linux/nicolina.nix
+++ hosts/x86_64-linux/nicolina.nix
@@ -54,6 +54,10 @@
       file = ../../secrets/exa-api-key.age;
       owner = "${toString adminUser.uid}";
     };
+    proton-pass-agent-token = {
+      file = ../../secrets/proton-pass-agent-token.age;
+      owner = "${toString adminUser.uid}";
+    };
   };
 
   networking.firewall = {
