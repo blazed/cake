92e83fc40f039ea3f8f84913c2e626f7f725347e
Boberg <1823919+blazed@users.noreply.github.com>
2026-09-04 21:11:33 +0200

    chore(flake): update inputs and beekeeper-studio
    Signed-off-by: Boberg <1823919+blazed@users.noreply.github.com>

--- flake/setup.nix
+++ flake/setup.nix
@@ -18,7 +18,7 @@
         inherit system;
         config.allowUnfree = true;
         config.permittedInsecurePackages = [
-          "beekeeper-studio-5.5.7"
+          "beekeeper-studio-6.0.5"
         ];
         overlays = [
           inputs.agenix.overlays.default
